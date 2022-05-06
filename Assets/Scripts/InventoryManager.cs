e9f8eab83b295c2a7f051684a5d6533e544b4519
Samuel Souza <sbjs@cin.ufpe.br>
2022-05-05 21:56:05 -0300

    Add inventory to multiple levels

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -5,31 +5,42 @@ using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
+    public GameObject FakeWall;
+    public List<Vector3> letterPositionMatrix = new List<Vector3>();
+
     public List<char> inventoryList = new List<char>();
     public int numberOfLetters = 3;
     public string word = "BAG";
     private Canvas canvas;
+    public List<Vector2> letterPositionList = new List<Vector2>();
     private TextMeshPro mesh;
     private GameObject objectChild;
     private char letterToAdd;
 
+    void Start()
+    {
+        setLetterPosition();
+    }
+
     public void PickupLetter(GameObject obj)
     {   
-        Debug.LogWarning($"Teste 3: {obj.transform.GetChild(0).GetChild(0).gameObject.tag}");
         objectChild = obj.transform.GetChild(0).GetChild(0).gameObject;
 
-        if (obj.transform.GetChild(0).GetChild(0).gameObject.tag == "LetterText")
+        if (objectChild.tag == "LetterText")
         {
             letterToAdd = objectChild.GetComponentInChildren<TextMeshProUGUI>().text.ToCharArray()[0];
             inventoryList.Add(letterToAdd);
             if(!LevelFinished() && LevelWronglyFinished())
             {
-                inventoryList.Clear();
+                // Game over
             } else if(LevelFinished()) {
-                // Finalizar o level
+                // Won
+                FakeWall.SetActive(false);
+            } else {
+                // Playing
+                UpdateInventoryLetters();
+                obj.transform.localScale = new Vector3(0, 0, 0);
             }
-            UpdateInventoryLetters();
-            Destroy(obj);
         }
     }
 
@@ -72,9 +83,47 @@ public class InventoryManager : MonoBehaviour
         return false;
     }
 
-    public void clearBag()
+    public void RestartLetters()
     {
+        var letterList = GameObject.FindGameObjectsWithTag("LetterContainer");
+         foreach(GameObject letterContainer in letterList)
+         {
+             Debug.LogWarning($"Loop ------>>>: {letterContainer.tag}");
+             letterContainer.transform.localScale = new Vector3(1, 1, 1);
+         }
+    }
+
+    public void RestartPlayerPosition() 
+    {
+        var player = GameObject.FindGameObjectsWithTag("Player")[0];
+        player.transform.position = new Vector3(-15, 8, 0);
+    }
+
+    public void ClearBag()
+    {
+        Debug.LogWarning($"Clear Bag click ------>>>");
         inventoryList.Clear();
+        RestartPlayerPosition();
         UpdateInventoryLetters();
+        RandomLetterPositionList();
+        RestartLetters();
+    }
+
+    private void RandomLetterPositionList()
+    {
+        var letterList = GameObject.FindGameObjectsWithTag("LetterContainer");
+        for(int i = 0; i < numberOfLetters; i++)
+        {
+            int num = Random.Range(i*numberOfLetters, (i*numberOfLetters)+2);
+            letterList[i].transform.position = letterPositionMatrix[num];
+        }
+    }
+
+    private void setLetterPosition()
+    {   var letterList = GameObject.FindGameObjectsWithTag("LetterContainer");
+        foreach(GameObject letterContainer in letterList)
+        {
+            letterPositionList.Add(letterContainer.transform.position);
+        }
     }
 }
